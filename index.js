1df16ef82a773e2959c24f7b47a0d90d3a29e05b
brayo-tech <oumabriansoja@gmail.com>
2025-09-29 19:08:26 +0300

    index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ https://www.youtube.com/@brian_tech_space
 https://github.com/briantechspace/Brian-XD
 
 > © ᴘᴏᴡᴇʀᴇᴅ ʙʏ BRIAN TECH SPACE`;
-    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/lm4a0b.jpg` }, caption: up })
+    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/dys87v.png` }, caption: up })
 
           const channelJid = "120363402507750390@newsletter"
           try {
